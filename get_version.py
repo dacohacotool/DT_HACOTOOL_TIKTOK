3e98c81ff03d2628b12e19c3556207aa16e98ba8
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-01-12 01:13:02 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,3 +1,3 @@
 {
-  "version":"Support Only Base 1.6. If not run on base1.6 please dont update"
+  "version":"[Update For Based 1.7] Fix config (doi_acc_khi_lam_duoc_so_jobs), Add New Config Options, optimal automatic, Add Tiktok Korea Language, Add More Limit,..."
 }
