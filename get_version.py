61b2ced85df630651743f57d901177f1d676b177
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-01-12 10:58:43 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,3 +1,3 @@
 {
-  "version":"[Update For Based 1.7] Fix config (doi_acc_khi_lam_duoc_so_jobs), Add New Config Options, optimal automatic, Add Tiktok Korea Language, Add More Limit,..."
+  "version":"[Update For Based 1.7] Fix Auto Update, Fix Follow api"
 }
