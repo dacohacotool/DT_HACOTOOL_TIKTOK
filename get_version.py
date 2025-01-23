1dafe72ce0631f950990bc84a2d6b811af166e75
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-01-24 01:16:14 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,4 +1,4 @@
 {
   "for_version": 1.9,
-  "version": "#PATH 302824: Fix interface freeze error\nFix unconfigured account error\nFix Tool Crash error\nAdd batch run feature\nSuper good multithreading optimization\n\nMore Info in t.me/haco_official"
+  "version": "#PATH 302824: Fix interface freeze error\nFix unconfigured account error\nFix Tool Crash error\nAdd batch run  feature\nSuper good multithreading optimization\n\nMore Info in t.me/haco_official"
 }
