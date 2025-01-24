2fb2a3ad5952ca940e116049685843b2bf052bae
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-01-24 12:23:18 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,4 +1,4 @@
 {
   "for_version": 1.9,
-  "version": "#PATH 302827: \nFSửa lỗi hoàn thành jobs \n\nMore Info in t.me/haco_official"
+  "version": "#PATH 302827: \nSửa lỗi hoàn thành jobs \n\nMore Info in t.me/haco_official"
 }
