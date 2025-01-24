3d3f10e0b2cc9f8ec573d0f342d5847daf95a777
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-01-24 12:22:39 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,4 +1,4 @@
 {
   "for_version": 1.9,
-  "version": "#PATH 302826: \nFix interface freeze error\nFix unconfigured account error\nFix Tool Crash error\nAdd batch run  feature\nSuper good multithreading optimization\nUpdate the list of violating words\nAdded feature: Delay get jobs\nFix virtual notification error\n\nMore Info in t.me/haco_official"
+  "version": "#PATH 302827: \nFSửa lỗi hoàn thành jobs \n\nMore Info in t.me/haco_official"
 }
