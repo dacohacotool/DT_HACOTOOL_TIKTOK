ab53846f175092d6c5b06c864450a0a406c51ddb
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-02-21 18:44:24 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,4 +1,4 @@
 {
   "for_version": 2.4,
-  "version": "#PATH302839:Fix Crash; More Info in t.me/haco_official"
+  "version": "#PATH302839:Open Deeplink tiktok, Turn on screen and disable rote, disable screentimout; More Info in t.me/haco_official"
 }
