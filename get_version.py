d550cc733fc14155fa13d0f1b4d580fcc791469c
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-03-10 12:25:19 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,4 +1,4 @@
 {
   "for_version": 2.4,
-  "version": "#PATH302842: (Vui lòng update) Fix get account v4, Add More Ads; More Info in t.me/haco_official "
+  "version": "#PATH302842: (Vui lòng update) Fix get account v4, Add More Ads (không khó chịu); More Info in t.me/haco_official "
 }
