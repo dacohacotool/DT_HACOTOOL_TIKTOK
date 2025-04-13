efdf9ad25caf9202ab98557e152901f3948eb52b
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-04-14 00:22:58 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,4 +1,4 @@
 {
-  "for_version": 2.4,
-  "version": "#PATH302844: Fix lỗi lấy danh sách tài khoản thất bại; More Info in t.me/haco_official "
+  "for_version": 2.5,
+  "version": "#PATH302845: Fix lỗi check comment; More Info in t.me/haco_official "
 }
