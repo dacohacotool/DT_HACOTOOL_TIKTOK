273ae99b9bc78133d11319ce0aee64e553c07dcb
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-04-14 19:04:20 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,4 +1,4 @@
 {
   "for_version": 2.5,
-  "version": "#PATH302845: Fix lỗi check comment; More Info in t.me/haco_official "
+  "version": "#PATH302848: Fix lỗi check comment; More Info in t.me/haco_official "
 }
