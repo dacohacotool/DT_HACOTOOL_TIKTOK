eb54b484595d6891586c4ec2aa3936985cc0a461
dacohacotool <155040032+dacohacotool@users.noreply.github.com>
2025-04-18 02:01:22 +0700

    Update get_version.py

--- get_version.py
+++ get_version.py
@@ -1,4 +1,4 @@
 {
   "for_version": 2.5,
-  "version": "#PATH302850: Thêm tính năng tìm kiếm account, Unfollow tiktok; More Info in t.me/haco_official "
+  "version": "#PATH302851: Fix lỗi mất data, Thêm tính năng tìm kiếm account, Unfollow tiktok, Open Public Likes; More Info in t.me/haco_official "
 }
